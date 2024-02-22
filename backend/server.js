ffb157869dd5613f586ff3aa053ca5ee09f8dd6a
JuHan Park <iiiamst16@gmail.com>
2024-02-22 21:37:42 +0900

    papago API 프론트엔드 연동 완료

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,44 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require("dotenv").config();
+
+let corsOptions = {
+  origin: 'http://localhost:5173',
+  credential: true
+}
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+const client_id = process.env.PAPAGO_CLIENT_ID;
+const client_secret = process.env.PAPAGO_CLIENT_SECRET
+
 app.listen(8080, () => {
   console.log('8080 포트에서 서버 실행중');
 })
 
-const client_id = process.env.PAPAGO_CLIENT_ID;
-const client_secret = process.env.PAPAGO_CLIENT_SECRET
-let query = "만나서 반갑습니다. 배고픈데 밥 먹으러 가실래요?";
-app.get('/translate', function (req, res) {
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+
+app.post('/papago/translate', function (req, res) {
   const api_url = 'https://openapi.naver.com/v1/papago/n2mt';
-  const data = JSON.stringify({ source: "ko", target: "en", text: query})
-  console.log(data);
+  const data = JSON.stringify({ source: "ko", target: "en", text: req.body.content })
   const opt = {
     method: "POST",
     headers: {
-      'Content-Type' : 'application/json; charset=UTF-8',
+      'Content-Type': 'application/json; charset=UTF-8',
       'X-Naver-Client-Id': client_id,
       'X-Naver-Client-Secret': client_secret
     },
     body: data
   }
   fetch(api_url, opt)
-    .then(async (res) => {
-      console.log(await res.json());
+    .then(async(response) => {
+      res.send(await response.json());
     })
     .catch((err) => {
       console.error(err)
     })
 });
-
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-})
--- frontend/src/pages/Papago.tsx
+++ frontend/src/pages/Papago.tsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 type Props = {};
 
 const Papago = (props: Props) => {
+  const [beforeTranslatedText, setBeforeTranslatedText] = useState<string>('')
+  const [afterTranslatedText, setAfterTranslatedText] = useState<string>('')
+
+  const translate = async () => {
+    const response = await axios.post(
+      "http://localhost:8080/papago/translate",
+      {
+        content: beforeTranslatedText
+      }
+    );
+    setAfterTranslatedText(response.data.message.result.translatedText);
+  };
   return (
     <div className="container mx-auto h-screen flex">
       <section className="w-1/2 h-full border">
         <div className="flex gap-5">
           <select>
-            <option selected>한국어</option>
-            <option>영어</option>
+            <option>한국어</option>
+            <option selected>영어</option>
             <option>일본어</option>
             <option>중국어 간체</option>
             <option>중국어 번체</option>
           </select>
-          <span>{'-->'}</span>
+          <span>{"-->"}</span>
           <select>
-            <option>한국어</option>
-            <option selected>영어</option>
+            <option selected>한국어</option>
+            <option>영어</option>
             <option>일본어</option>
             <option>중국어 간체</option>
             <option>중국어 번체</option>
@@ -28,12 +41,13 @@ const Papago = (props: Props) => {
             rows={10}
             cols={30}
             value={
-              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, illo nostrum veritatis, ex totam blanditiis placeat reiciendis qui ipsum at alias accusamus, voluptas doloremque magni necessitatibus dicta mollitia suscipit culpa."
+              beforeTranslatedText
             }
+            onChange={(e) => setBeforeTranslatedText(e.target.value)}
           />
         </div>
         <div>
-          <button>번역하기</button>
+          <button onClick={translate}>번역하기</button>
         </div>
       </section>
       <section className="w-1/2 h-full border">
@@ -41,10 +55,7 @@ const Papago = (props: Props) => {
           <span>결과</span>
         </div>
         <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-          corporis quasi! Consectetur tempore pariatur molestias, fuga veritatis
-          iure optio modi nemo expedita magnam eos nostrum voluptatum at
-          distinctio rerum illo!
+          {afterTranslatedText}
         </div>
       </section>
     </div>
