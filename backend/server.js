2a7acbc6bf7814f54882afd17cbefc46c6e03643
JuHan Park <iiiamst16@gmail.com>
2024-02-23 23:39:36 +0900

    계정 생성 API 간단하게 구현

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,26 @@ app.post('/papago/translate', function (req, res) {
     body: data
   }
   fetch(api_url, opt)
-    .then(async(response) => {
+    .then(async (response) => {
       res.send(await response.json());
     })
     .catch((err) => {
       console.error(err)
     })
 });
+
+let users = [];
+
+app.post('/user/register', (req, res) => {
+  const { id, password, name } = req.body;
+  if (!id || !password || !name) {
+    res.status(400).send({ message: '필수 입력사항을 제대로 입력해주세요.' });
+    return;
+  }
+  if (users.find((user) => user.id === id)) {
+    res.status(400).send({ message: '아이디가 중복됩니다. 다른 아이디를 사용해주세요.' });
+    return;
+  }
+  users.push({ id, password, name });
+  res.status(200).json({ message: '계정 생성 완료' });
+})
