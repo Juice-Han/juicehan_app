5fe2f6839e7a539fb15127b67ec78deb2a8022dd
JuHan Park <iiiamst16@gmail.com>
2024-02-24 12:34:44 +0900

    메인페이지 헤더 추가

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
 
 type Props = {}
 
 const MainPage = (props: Props) => {
   return (
+    <>
+    <Header/>
     <div>MainPage</div>
+    </>
   )
 }
 
